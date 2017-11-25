d1ceb960705ece91c499b226213966aadfe4684c
ra21708493 <ra21708493@AN0700120077507.dinfor.uniceub>
2017-11-24 22:30:59 -0200

    Commit final

--- Prova2/src/main/Principal.java
+++ Prova2/src/main/Principal.java
@@ -8,10 +8,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// Gabriel Gago Duro Calcagno RA:21708493
 		BD bds = new BD();
-		//ControlePesquisaEsporte cpe = new ControlePesquisaEsporte(bds);
-		//cpe.exibirEsportes();
-		
-		System.out.println(bds.getModalidadePorNome("boxe"));
+		ControlePesquisaEsporte cpe = new ControlePesquisaEsporte(bds);
+		cpe.exibirEsportes();
 
 	}
 
--- Prova2/src/view/ViewExibeModalidades.java
+++ Prova2/src/view/ViewExibeModalidades.java
@@ -7,14 +7,10 @@ import model.Esporte;
 public class ViewExibeModalidades {
 
 	public ViewExibeModalidades(ArrayList<Esporte> esportes) {
+
 		for (int i = 0; i < esportes.size(); i++) {
 			System.out.println("Modalidade: " + esportes.get(i).getModalidade());
-
 		}
-	}
-
-	public ViewExibeModalidades(String es) {
-		// TODO Auto-generated constructor stub
 	}
 
 }
